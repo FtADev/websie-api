d250be81e6901c3d90fae64f3f5522744ae852f6
fatemeh <akhlaghi.fatemeh@gmail.com>
2023-12-27 12:14:47 +0330

    + Project Service Impl

---
+++ src/main/java/com/example/websiteapi/service/ProjectServiceImpl.java
@@ -0,0 +1,68 @@
+package com.example.websiteapi.service;
+
+import com.example.websiteapi.doa.ProjectRepository;
+import com.example.websiteapi.entity.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+    private ProjectRepository projectRepository;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public List<Project> findAll() {
+        return projectRepository.findAllByOrderByPriorityAsc();
+    }
+
+    @Override
+    public Project findById(int id) {
+        Optional<Project> result = projectRepository.findById(id);
+
+        Project project = null;
+
+        if (result.isPresent()) {
+            project = result.get();
+        }
+        else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find project id - " + id);
+        }
+
+        return project;
+    }
+
+    @Override
+    public void save(Project employee) {
+        projectRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        projectRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Project> searchBy(String theName) {
+
+        List<Project> results = null;
+
+        if (theName != null && (theName.trim().length() > 0)) {
+            results = projectRepository.findByFirstNameContainsOrLastNameContainsAllIgnoreCase(theName, theName);
+        }
+        else {
+            results = findAll();
+        }
+
+        return results;
+    }
+}
+
+
+
