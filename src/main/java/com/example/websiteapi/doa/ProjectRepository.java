71de93072918370cc938d36087c54982e19791f7
fatemeh <akhlaghi.fatemeh@gmail.com>
2023-12-27 12:14:24 +0330

    + Project repository

---
+++ src/main/java/com/example/websiteapi/doa/ProjectRepository.java
@@ -0,0 +1,16 @@
+package com.example.websiteapi.doa;
+
+import com.example.websiteapi.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+    //JPA will understand the pattern from method name
+    // In this method, JPA will call "from Project order by priority asc"
+    public List<Project> findAllByOrderByPriorityAsc();
+
+    // search by name
+    public List<Project> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+}
